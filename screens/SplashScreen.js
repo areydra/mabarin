0bbb5b14e3e339dbfe8e7810f053c1bf18ba7914
Oreki13 <arfandynugraha21@gmail.com>
2019-10-13 13:36:27 +0700

    fix login register

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,22 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import {SafeAreaView, StyleSheet, Image} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import firebase from 'firebase';
 
 const SplashScreen = props => {
-  const [uid, setUid] = useState([null]);
-
-  const Check = async () => {
-    await setUid(AsyncStorage.getItem('uid'));
-    if (uid !== null) {
-      props.navigation.navigate('Login');
-    } else {
-      props.navigation.navigate('Home');
-    }
-  };
-
   useEffect(() => {
     setTimeout(() => {
-      Check();
+      props.navigation.navigate('Login');
     }, 2000);
   }, []);
 
