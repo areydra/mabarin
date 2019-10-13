4186ff9f93404bb005d5f28969e594a7a04489c0
Areydra Desfikriandre <areydra@gmail.com>
2019-10-13 10:25:55 +0700

    add key firebase

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import Router from './routes'
+import firebase from 'firebase'
+
+import { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId } from './key'
+
+firebaseConfig  = {
+  apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId
+}
+
+firebase.initializeApp(firebaseConfig)
 
 const App = () => {
   return (
---
+++ key.js
@@ -0,0 +1,11 @@
+const apiKey= "AIzaSyCxyUJ-wFVTPr3YuRoMx48IENGHVQIkH3Y"
+const authDomain= "mabarin-5d1f7.firebaseapp.com"
+const databaseURL= "https://mabarin-5d1f7.firebaseio.com"
+const projectId= "mabarin-5d1f7"
+const storageBucket= "mabarin-5d1f7.appspot.com"
+const messagingSenderId= "905201801678"
+const appId= "1:905201801678:web:a1deb0bf6f1a1d9883339f"
+
+export {
+    apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId
+}
