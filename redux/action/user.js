7c1b1e84d90af1dbafa99e90969d4a8ea4901486
Areydra Desfikriandre <areydra@gmail.com>
2019-10-17 16:26:20 +0700

    rating cek

--- redux/action/user.js
+++ redux/action/user.js
@@ -4,7 +4,7 @@ export const getUser = uid => {
   return {
     type: 'GET_USER',
     payload: Axios.get(
-      `http://18.141.12.25:6969/api/users/uid/pwdj337j3k3bj3b3rkrr`,
+      `http://18.141.12.25:6969/api/users/uid/${uid}`,
     ),
   };
 };
